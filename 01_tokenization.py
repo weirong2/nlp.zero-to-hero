59205997ccc6b07a3fa019152a8c82bcc9b137c7
Wei Rong <rongwei_hm@hotmail.com>
2024-08-22 23:21:45 +0800

    initial commit

---
+++ 01_tokenization.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentences = [
+    'I love my dog',
+    'I love my cat',
+    'You love my dog!',
+    'Do you think my dog is amazing?'
+]
+
+tokenizer = Tokenizer(num_words = 100, oov_token="<OOV>")
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+
+sequences = tokenizer.texts_to_sequences(sentences)
+
+padded = pad_sequences(sequences)
+
+print(word_index)
+print(sequences)
+print(padded)
---
+++ README.md
@@ -0,0 +1,3 @@
+NLP Zero to Hero
+
+https://www.youtube.com/playlist?list=PLQY2H8rRoyvzDbLUZkbudP-MFQZwNmU4S
